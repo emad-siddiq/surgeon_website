a1df86da0fee2c6e3fe5f55886141cd9ec5af5bf
Emad <emadsiddiq@berkeley.edu>
2024-12-20 01:34:21 +0500

    save changes to before/after component

--- frontend/src/components/web/06_ServiceOfferings/ServiceOfferings.jsx
+++ frontend/src/components/web/06_ServiceOfferings/ServiceOfferings.jsx
@@ -5,13 +5,13 @@ import './ServiceOfferings.css';
 const ServiceOfferings = () => {
   // Predefined list of counts that can be easily modified
   const serviceCounts = [
-    85, 92, 78, 65, 
+    9000, 8000, 1500, 65, 
     88, 79, 63, 71, 
     59, 82
   ];
 
   const surgicalServices = [
-    { name: 'Gall Bladder Surgery', count: serviceCounts[0], speed: 100 },
+    { name: 'Gall Bladder Surgery', count: serviceCounts[0], speed: 50 },
     { name: 'Appendix Surgery', count: serviceCounts[1], speed: 120 },
     { name: 'Laparoscopic Surgery', count: serviceCounts[2], speed: 80 },
     { name: 'Colon Surgery', count: serviceCounts[3], speed: 90 },
